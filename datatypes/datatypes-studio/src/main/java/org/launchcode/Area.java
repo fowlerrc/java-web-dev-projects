bdbc1305b37cab724fd9d567474abdafa6f8efdc
fowlerrc <142541725+fowlerrc@users.noreply.github.com>
2024-04-01 20:12:29 -0500

    Class 1 Studio

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner radInput;
+        radInput = new Scanner(System.in);
+        System.out.println("input the radius of a circle: ");
+        double radius = radInput.nextDouble();
+        double area = Circle.getArea(radius);
+        System.out.println(("The circle's area is: "+area));
+    }
+}
